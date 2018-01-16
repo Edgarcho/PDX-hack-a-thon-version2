8cd898113e5cd7f5959e2689deb22afcead89527
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-16 15:08:28 -0800

    Getter method for name field passed test.

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,9 +1,12 @@
 package models;
 
 public class Team {
+    private String name;
+
     public Team(String name, String description) {
+        this.name = name;
     }
     public String getName(){
-        return null;
+        return name;
     }
 }
