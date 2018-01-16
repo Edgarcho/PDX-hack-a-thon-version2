3773433172f6e9f0bfdc75b4d0d6bdd110bc9be6
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-16 14:54:48 -0800

    Created first test.

--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,11 @@ public class TeamTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void newTeam_instantiatesCorrectly() throws Exception {
+        Team testTeam = new Team("The A Team","First time at a Hack-a-tron");
+        assertEquals(true, testTeam instanceof Team);
     }
 }
