8d17fb3e4804e604386f6b5047ad0ebf94a545a6
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-17 15:20:11 -0800

    Change templates layout.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,10 +10,24 @@ import static spark.Spark.get;
 import static spark.Spark.post;
 import static spark.Spark.staticFileLocation;
 
+
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
+        //get: show new team form
+        get("/teams/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "newteam-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: show new member form
+        get("/teams/member",(request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "member-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: show all teams
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             ArrayList<Team> teams = Team.getAll();
