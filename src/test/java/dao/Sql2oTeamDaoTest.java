d3b4c16fba2eb0428be0efbaba11e60fc85c2d18
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-19 13:25:08 -0800

    Created first test, adding a Team and successfully setting a Team's id

--- src/test/java/dao/Sql2oTeamDaoTest.java
+++ src/test/java/dao/Sql2oTeamDaoTest.java
@@ -1,8 +1,13 @@
+import models.Team;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.sql2o.Sql2o;
 
+import javax.swing.tree.ExpandVetoException;
 import java.sql.Connection;
+
+import static org.junit.Assert.assertNotEquals;
 
 public class Sql2oTeamDaoTest {
     private Sql2oTeamDao teamDao;
@@ -20,5 +25,13 @@ public class Sql2oTeamDaoTest {
     @After
     public void tearDown() throws Exception {
         conn.close();
+    }
+
+    @Test
+    public void addingCourseSetsId() throws Exception {
+        Team team = new Team("A Team","Group of one");
+        int originalTeamId = team.getId();
+        teamDao.add(team);
+        assertNotEquals(originalTeamId, team.getId());
     }
 }
