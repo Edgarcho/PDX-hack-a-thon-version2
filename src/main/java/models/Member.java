c5f6482147c3e6620f1b1fe380ed8f087e505e73
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-22 13:16:52 -0800

    Generate setters methods for Member class

---
+++ src/main/java/models/Member.java
@@ -0,0 +1,32 @@
+package models;
+
+public class Member {
+    private String name;
+    private int id;
+
+    public Member(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        if (id != member.id) return false;
+        return name.equals(member.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + id;
+        return result;
+    }
+}
