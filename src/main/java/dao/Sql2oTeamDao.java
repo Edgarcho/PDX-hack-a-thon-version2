b114bc11ae347f8b8966ee58c8be662289cf008b
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-21 18:26:48 -0800

    Test passed

--- src/main/java/dao/Sql2oTeamDao.java
+++ src/main/java/dao/Sql2oTeamDao.java
@@ -11,14 +11,14 @@ public class Sql2oTeamDao implements TeamDao {
 
     private final Sql2o sql2o;
 
-    public Sql2oTeamDao(Sql2o sql2o){
+    public Sql2oTeamDao(Sql2o sql2o) {
         this.sql2o = sql2o;
     }
 
     @Override
     public void add(Team team) {
         String sql = "INSERT INTO teams (name, description) VALUES (:name, :description)";
-        try(Connection con = sql2o.open()){
+        try (Connection con = sql2o.open()) {
             int id = (int) con.createQuery(sql)
                     .bind(team)
                     .executeUpdate()
@@ -31,7 +31,7 @@ public class Sql2oTeamDao implements TeamDao {
 
     @Override
     public List<Team> getAll() {
-        try(Connection con = sql2o.open()){
+        try (Connection con = sql2o.open()) {
             return con.createQuery("SELECT * FROM teams")
                     .executeAndFetch(Team.class);
         }
@@ -39,7 +39,7 @@ public class Sql2oTeamDao implements TeamDao {
 
     @Override
     public Team findById(int id) {
-        try(Connection con = sql2o.open()){
+        try (Connection con = sql2o.open()) {
             return con.createQuery("SELECT * FROM teams WHERE id = :id")
                     .addParameter("id", id)
                     .executeAndFetchFirst(Team.class);
@@ -49,12 +49,25 @@ public class Sql2oTeamDao implements TeamDao {
     @Override
     public void update(int id, String name) {
         String sql = "UPDATE teams SET name = :name WHERE id=:id";
-        try(Connection con = sql2o.open()){
+        try (Connection con = sql2o.open()) {
             con.createQuery(sql)
                     .addParameter("name", name)
-                    .addParameter("id",id)
+                    .addParameter("id", id)
                     .executeUpdate();
-        } catch (Sql2oException ex){
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void updateDescription(int id, String description) {
+        String sql = "Update teams SET description = :description WHERE id=:id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("description", description)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
             System.out.println(ex);
         }
     }
