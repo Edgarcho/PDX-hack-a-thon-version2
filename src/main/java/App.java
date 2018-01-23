2bd1e66322c2e25fdcf57e6ac55f5e0ae3455fef
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-23 10:57:01 -0800

    Edit layout template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,14 @@
+import dao.Sql2oMemberDao;
+import dao.Sql2oTeamDao;
+import models.Member;
 import models.Team;
+import org.sql2o.Sql2o;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
@@ -12,15 +17,62 @@ import static spark.Spark.staticFileLocation;
 
 
 public class App {
-    /*
     public static void main(String[] args) {
         staticFileLocation("/public");
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        Sql2oMemberDao memberDao = new Sql2oMemberDao(sql2o);
+        Sql2oTeamDao teamDao = new Sql2oTeamDao(sql2o);
 
-        //get: show new team form
+        //get: show all teams
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            List<Team> allTeams = teamDao.getAll();
+            model.put("teams", allTeams);
+            List<Member>members = memberDao.getAll();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+
+    //get a specific team (and the members it contains)
+    // /teams/:team_id
+
+    //get: show a specific member in a specific team
+    // /teams/:team_id/members/:member_id
+
+    //get: show a form to create a new team
+    //  /teams/new
+
+    //post: process a form to create a new team
+    //  /teams
+
+    //get: show a form to update team
+    //  /teams/update
+
+    //post: process a form to update team
+    //  /teams/update
+
+    //get: show a form to update description in team
+    //  /teams/updateDescription
+
+    //post: process a form to update description in team
+    //  /teams/updateDescription
+
+    //get: delete a team and members it contains
+    //  /teams/:teams_id/delete
+
+    //get: delete all teams and all members
+
+    //get: show all tasks in all categories and show all categories
+
+
+
+    /*
         get("/teams/new", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "team-form.hbs");
         }, new HandlebarsTemplateEngine());
+
 
         //post: process new team form
         post("/teams/new", (request, response) -> {
